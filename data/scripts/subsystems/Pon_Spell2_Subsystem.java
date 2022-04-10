6ecf18acb14642b0b112fd12b465958cec43810d
Dmzori <noahhoffer@gmail.com>
2022-04-10 11:48:15 -0600

    Shield spell done and code cleaned up

--- data/scripts/subsystems/Pon_Spell2_Subsystem.java
+++ data/scripts/subsystems/Pon_Spell2_Subsystem.java
@@ -1,14 +1,10 @@
 package data.scripts.subsystems;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.SoundPlayerAPI;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipwideAIFlags;
-import com.fs.starfarer.api.combat.WeaponAPI;
-import static data.scripts.subsystems.dl_BaseSubsystem.SubsystemState.ACTIVE;
 import java.awt.Color;
-import java.util.List;
 import org.lwjgl.util.vector.Vector2f;
 
 
@@ -34,33 +30,31 @@ public class Pon_Spell2_Subsystem extends dl_BaseSubsystem
             return;
         }
         
-
-      
         float shieldArc = ship.getHullSpec().getShieldSpec().getArc();//ships original shield arc
         Color innerShieldColor = ship.getHullSpec().getShieldSpec().getInnerColor();//ships original inner shield color
         Color ringShieldColor = ship.getHullSpec().getShieldSpec().getRingColor();//ships original ring color
         Color activeShieldColor = new Color(0, 100, 0);//inner shield color
         Color activeRingShieldColor = new Color(200, 200, 200);//outer ring color
-        SoundPlayerAPI soundPlayer = Global.getSoundPlayer();
-        
         stats.getShieldTurnRateMult().modifyMult(id, 1f);
         stats.getShieldUnfoldRateMult().modifyPercent(id, 150);//2000 originally
         stats.getShieldDamageTakenMult().modifyMult(id, 1f - DAMAGE_MULT * effectLevel);
 	stats.getShieldDamageTakenMult().modifyMult(id, 0.1f);
         stats.getShieldUpkeepMult().modifyMult(id, 0f);
-        //ship.getShield().setArc(360f);
         if(isActive() || isFadingIn()){
             ship.getShield().toggleOn();
             ship.getShield().setArc(360f);
             ship.getShield().setInnerColor(activeShieldColor);
             ship.getShield().setRingColor(activeRingShieldColor);
+                Global.getSoundPlayer().playLoop("system_fortress_shield_loop", 
+                                                  ship,
+                                                  1f,
+                                                  1f,
+                                                  ship.getLocation(),
+                                                  new Vector2f()); 
+//playloop(sound ID, object playing, pitch, volume, location, velocity)
             
-                //Global.getLogger(this.getClass()).debug("wrong thing");
-                Global.getSoundPlayer().playLoop("system_fortress_shield_loop", ship, 1f, 1f, ship.getLocation(), new Vector2f()); //playloop(sound ID, object playing, pitch, volume, location, velocity)
-                //Global.getSoundPlayer().applyLowPassFilter(0.75f, 0.0f); 
-            //}
-            
-        }else if (isFadingOut()){
+        }else if (isFadingOut())
+        {
             //ship.getShield().toggleOff();
             ship.getShield().setArc(shieldArc);
             ship.getShield().setInnerColor(innerShieldColor);
